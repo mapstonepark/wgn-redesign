435bcc5265d455222ac0bfc77830a5eadfbd0a7f
Jamal Park <102711674+mapstonepark@users.noreply.github.com>
2023-07-06 22:03:00 -0500

    header image

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,15 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Header() {
   return (
-    <div className='test'>Header Test</div>
+    <div className='test'>
+    <Image
+      src='/Web_Header_Filled.png'
+      fill={true} // {true} | {false}
+      alt='WGN Radio Logo'
+      />
+      </div>
   )
 }
 
